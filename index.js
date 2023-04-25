d32c6d55a9abafdaec4f0beb37b50191733528e8
Matheus Carvalho <matheusc200@gmail.com>
2023-04-25 19:40:44 -0300

    Adicionei uns campos e um input com js

---
+++ index.js
@@ -0,0 +1,6 @@
+const submitBTN = document.querySelector('#submit-btn');
+
+submitBTN.addEventListener('click', (event) => {
+    event.preventDefault();
+    alert('Formulário enviado com sucesso!');
+    });
