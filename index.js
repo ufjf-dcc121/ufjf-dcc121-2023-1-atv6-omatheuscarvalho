d3deec4e4ba4f5dbd89211efaccd0334994d5e41
Matheus Carvalho <matheusc200@gmail.com>
2023-04-25 20:19:31 -0300

    Adicionado css e a funcao do botao

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const submitBTN = document.querySelector('#submit-btn');
-
-submitBTN.addEventListener('click', (event) => {
-    event.preventDefault();
-    alert('Formulário enviado com sucesso!');
-    });
-
-const pessoa = {
+let pessoa = {
     nome: "Matheus",
     idade: 20,
     cidade: "Juiz de Fora",
-    estado: "MG"
+    estado: "MG",
+    envelhecer: function() {
+        this.idade++;
+    }
 }
+const btn = document.querySelector('#btn');
+const nome = document.querySelector('#nome');
+const idade = document.querySelector('#idade');
+
+nome.innerHTML = `Nome: ${pessoa.nome}`;
+idade.innerHTML = `Idade: ${pessoa.idade}`	;
+
+const atualizaIdade = () => {
+    idade.innerHTML = `Idade: ${pessoa.idade}`;
+}
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    pessoa.envelhecer();
+    atualizaIdade();
+    //alert(`A pessoa ${pessoa.nome} envelheceu!`)
+    });
