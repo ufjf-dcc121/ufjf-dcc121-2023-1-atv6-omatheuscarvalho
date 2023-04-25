b850baa1202f7d5e89b2be1789ff83c4af65f018
Matheus Carvalho <matheusc200@gmail.com>
2023-04-25 20:00:55 -0300

    Adicionei pessoa

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ submitBTN.addEventListener('click', (event) => {
     event.preventDefault();
     alert('Formulário enviado com sucesso!');
     });
+
+const pessoa = {
+    nome: "Matheus",
+    idade: 20,
+    cidade: "Juiz de Fora",
+    estado: "MG"
+}
